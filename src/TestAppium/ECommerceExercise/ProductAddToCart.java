6921ac1e367fbd1a513216e5037a262f56bc6547
EybTheGreat <abraham.morial.ora@gmail.com>
2021-04-09 11:23:29 +0800

    Change ==  to equalsIgnoreCAse

--- src/TestAppium/ECommerceExercise/ProductAddToCart.java
+++ src/TestAppium/ECommerceExercise/ProductAddToCart.java
@@ -25,21 +25,23 @@ public class ProductAddToCart extends BaseClass {
 //        driverAddToCart.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"));" ).isDisplayed();
 //        driverAddToCart.findElementByXPath("(//android.widget.TextView[@text='ADD TO CART'])[2]").click();
 
-
         driverAddToCart.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));
+
+        driverAddToCart.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
 
         int count = driverAddToCart.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
 
         for (int i = 0; i < count; i++) {
             String text = driverAddToCart.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
 
-            if (text.equals("Jordan 6 Rings")) {
+            if (text.equalsIgnoreCase("Jordan 6 Rings")) {
 
                 driverAddToCart.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
                 break;
             }
 
         }
+
 
         driverAddToCart.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
 
